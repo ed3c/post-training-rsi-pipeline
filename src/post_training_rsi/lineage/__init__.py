a744f0c0eb3de34843efe698e34d6a652a87964c
ed3c <mcnum01@gmail.com>
2026-08-14 03:03:26 +0800

    fix: expose only implemented lineage components

--- src/post_training_rsi/lineage/__init__.py
+++ src/post_training_rsi/lineage/__init__.py
@@ -1,5 +1,4 @@
-from .audit import AuditFinding, LineageAuditor
 from .manifest import LineageManifest
 from .store import ArtifactStore
 
-__all__ = ["ArtifactStore", "AuditFinding", "LineageAuditor", "LineageManifest"]
+__all__ = ["ArtifactStore", "LineageManifest"]
